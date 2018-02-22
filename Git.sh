3433ef058fb43a4ac6adbe7577cb63f3744e1c6d
cglin <cglin95@gmail.com>
2018-02-22 12:55:45 +0800

    [2018-02-22]

--- Git.sh
+++ Git.sh
@@ -2,7 +2,7 @@
 #A script to update the repository
 
 git status
-options=[Add/Commit/Diff/Exit/Log/Push/User/Status]
+options=[Add/Commit/Diff/Exit/Log/Push/User/Reset/Status]
 while echo -e -n "\n\nPlease choose the first letter of options.\n$options? "
 	read -n1 choice
 do
@@ -12,7 +12,7 @@ do
 		git add -A
 		git status;;
 	C | c)
-		echo -e -n "Enter the remark: "
+		echo -e -n "Please enter the remark: "
 		read commit
 		commitDate="[20"$(date +%y-%m-%d])
 		commit=$commitDate$commit
@@ -23,13 +23,19 @@ do
 	E | e)
 		exit 0;;
 	L | l)
+		echo "It only shows the logs for nearly two weeks."
 		git log --since=2.weeks --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit;;
 	P | p)
 	        git push origin master;;
 	U | u)
-		echo -e -n ">>"
+		echo "Please input user's instructions."
+		echo -e -n ">> "
 		read myCmd
 		$myCmd;;
+	R | r)
+		git log --since=2.weeks --color --graph --pretty=oneline
+		read -p "Please enter the reset_id: " reset_id
+		git reset --hard $reset_id;;
 	S | s)
 		git status;;
 	*)
