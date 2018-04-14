7309c0f1131810c97a79c8f9690d30eeca0804b2
cglin <cglin95@gmail.com>
2018-04-14 11:47:02 +0800

    [2018-04-14]modify Git.sh

--- Git.sh
+++ Git.sh
@@ -3,7 +3,7 @@
 
 git status
 options="[Add|Commit|Diff|Fetch|Exit|Help|Log|Push|User|Reset|Status]"
-while echo -e -n "\n\n\033[36mPlease choose the first letter of options.\n$options? \033[0m "
+while echo -e -n "\n\n\033[01;36mPlease choose the first letter of options.\n$options? \033[0m "
 	read -n1 choice
 do
 	echo -e "\n"
@@ -12,7 +12,7 @@ do
 		git add -A
 		git status;;
 	C | c)
-		echo -e -n "\033[36mPlease enter the remark: \033[0m "
+		echo -e -n "\033[01;36mPlease enter the remark: \033[0m "
 		read commit
 		commitDate="[20"$(date +%y-%m-%d])
 		commit=$commitDate$commit
@@ -24,13 +24,13 @@ do
 	F | f)
 		git fetch origin master:origin_master
 		git diff origin_master
-		echo -e -n "\033[36mAre you sure to merge with origin_master[Y/N] \033[0m "
+		echo -e -n "\033[01;36mAre you sure to merge with origin_master[Y/N] \033[0m "
 		read -n1 merge_choice
 		echo -e "\n"
 		if [[ $merge_choice == 'y' ]] || [[ $merge_choice == 'Y' ]]; then
 			git merge origin_master
 		else
-			echo -e -n "\033[36mHaved canceled the merge option.\n \033[0m "
+			echo -e -n "\033[01;36mHaved canceled the merge option.\n \033[0m "
 		fi
 		git branch -D origin_master;;
 	H | h)
@@ -45,13 +45,13 @@ do
 		echo "	R: Reset current version according version_id. "
 		echo "	S: Show status of repository. ";;
 	L | l)
-		echo -e -n "\033[36mIt only shows the logs for nearly two weeks. \033[0m "
+		echo -e -n "\033[01;36mIt only shows the logs for nearly two weeks. \033[0m "
 		git log --since=2.weeks --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit;;
 	P | p)
 	        git push origin master
 			git status;;
 	U | u)
-		echo -e "\033[36mPlease input user's instructions. \033[0m "
+		echo -e "\033[01;36mPlease input user's instructions. \033[0m "
 		while read -p ">> " myCmd
 		do
 			echo $myCmd
@@ -63,7 +63,7 @@ do
 		done;;
 	R | r)
 		git log --since=2.weeks --color --graph --pretty=oneline
-		echo -e -n "\033[36mPlease enter the reset_id: \033[0m "
+		echo -e -n "\033[01;36mPlease enter the reset_id: \033[0m "
 		read reset_id
 		git reset --hard $reset_id;;
 	S | s)
